4b6c123978ca0d3e773977af336e34a6f9e40c08
jcvh <jeanchristophevh@gmail.com>
2020-04-24 22:39:55 +0200

    Modification du HTML du site

---
+++ blog/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 23:28:45 2020
+
+@author: Jean-Christophe
+"""
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+]
